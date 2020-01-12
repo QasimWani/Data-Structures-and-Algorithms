fbf0ef83fb4859441ce509acbaf7b8168811e1b2
Qasim Wani <qasim31wani@gmail.com>
2020-01-12 23:29:18 +0300

    rte timing

--- Data Structures/Hash/API/main.cpp
+++ Data Structures/Hash/API/main.cpp
@@ -10,6 +10,8 @@ int main()
     Hash * phone = new Hash();
     long long number;
     string contactName;
+    clock_t t1,t2;
+    float diff;
     while (number != -1)
     {
         cout << "\nEnter Phone Number : ";
@@ -24,8 +26,10 @@ int main()
     }
     
     cout << "\nPrinting Name Based Hash Table\n" << endl;
-    
+    t1 = clock();
     phone->printNumHash();
+    t2 = clock();
+    
     cout << "\nPrinting Number Based Hash Table\n" << endl;
 
     phone->printNameHash();
@@ -37,5 +41,10 @@ int main()
     cout << "Find One Contact by Number: ";
     cin >> number;
     phone->findByNumber(number, true);
+
+    diff = ((float)t2-(float)t1);
+    cout<< "\n\nTime Difference : " << diff << "\n" << endl;
+
+    
     return 0;
 }
